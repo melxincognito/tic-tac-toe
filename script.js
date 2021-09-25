bf102fb8aaafb2c0f31e0a37ed5f8c1722c3fa21
Mel Incognito <melxincognito@gmail.com>
2021-09-24 17:19:14 -0700

    a fucking mess

--- script.js
+++ script.js
@@ -1,79 +1,103 @@
 (function () {
     var foo = "bar";
-
-    // Outputs: "bar"
     console.log(foo);
 })();
 
 
 function Player(name, marker) {
     this.name = name, 
-    this.marker = marker, 
+    this.marker = marker,
     this.playerInfo = () => {
         alert("NEW PLAYER: " + name.toUpperCase() + " MARKER: '" + marker.toUpperCase() + "'")
     }
 }
 
 
-(function getPlayer() {
-    var newPlayer = prompt('enter player name').toLowerCase();
-    var newMarker = prompt('choose your marker, x or o').toLowerCase();
+const board = (() => {
+    const boxes = Array.from(document.getElementsByClassName('box'));
+    const spaces = [];
+    const players = [];
 
-    var newPlayer = new Player(newPlayer, newMarker);
-    newPlayer.playerInfo();
+
+    return { boxes, spaces, players }
 })();
 
+const newPlayers = ((playerOne, playerTwo) => {
+
+    var playerOneName = prompt('enter player one name');
+    var playerOneMarker = 'O';
+    var playerOne = new Player(playerOneName, playerOneMarker);
+    var playerTwoName = prompt('enter player two name');
+    var playerTwoMarker = 'X';
+    var playerTwo = new Player(playerTwoName, playerTwoMarker);
+    board.players.push(playerOne, playerTwo)
+    return { playerOne, playerTwo }
+
+})();
+
+const playGame = () => {
+    board.boxes.forEach((box) => {
+        box.addEventListener('click', boxClicked);
+    });
+};
+
+const boxClicked = (e) => {
+    const id = e.target.id;
+
+    const markO = newPlayers.playerOne.marker;
+    const markX = newPlayers.playerTwo.marker;
+    let currentPlayer;
+    currentPlayer = currentPlayer === markO ? markX : markO ;
+
+    if(!board.spaces[id]){
+            board.spaces[id] = currentPlayer;
+            e.target.innerText = currentPlayer;
+            if(playerHasWon()) {
+                console.log(`${currentPlayer} has won!`);
+                return
+            }
+
+};
 
 
-
-
-const theGrid = (function setGrid() {
-    let board = new Array(9)
-
-    for (x = 0; x < 9; x++) {
-        var square = document.createElement('div')
-        square.classList.add('block');
-        square.setAttribute('id', 'gameBlock');
-        square.style.width = `200px`;
-        square.style.height = `200px`;
-        square.style.gridTemplateColumns = `repeat(3, 1fr)`;
-        square.style.gridTemplateRows = `repeat(3, 1fr)`;
-        square.style.position = 'relative'
-        board.push(square);
-        document.getElementById('test').appendChild(square)
+const playerHasWon = () => {
+    if(board.spaces[0] === currentPlayer) {
+        if(board.spaces[1] === currentPlayer && board.spaces[2] === currentPlayer) {
+            alert(`${currentPlayer} wins up top bitch`)
+            return true;
+        }
+        if(board.spaces[3] == currentPlayer && board.spaces[6] == currentPlayer) {
+            alert(`${currentPlayer} wins on left bitch`)
+            return true;
+        }
+        if(board.spaces[4] == currentPlayer && board.spaces[8] == currentPlayer) {
+            alert(`${currentPlayer} wins diagnal bitch`)
+            return true;
+        }
+    } else if(board.spaces[8] === currentPlayer) {
+        if(board.spaces[7] === currentPlayer && board.spaces[6] === currentPlayer) {
+            alert(`${currentPlayer} wins on bottom puta`)
+            return true;
+        }
+        if(board.spaces[2] == currentPlayer && board.spaces[5] == currentPlayer) {
+            alert(`${currentPlayer} wins on right putita`)
+            return true;
+        }
+    } else if(board.spaces[4] === currentPlayer) {
+        if(board.spaces[3] === currentPlayer && board.spaces[5] === currentPlayer) {
+            alert(`${currentPlayer} wins center horizantal`)
+            return true;
+        }
+        if(board.spaces[1] == currentPlayer && board.spaces[7] == currentPlayer) {
+            alert(`${currentPlayer} wins center vertical, hooyaa`)
+            return true;
+        }
     }
-    return {
-        square,
-    };
-
-})()
+ }
+}
 
 
 
-const Formatter = (function() {
-    const log = (message) => console.log(`[${Date.now()}] Logger: ${message}`);
-    const timesRun = [];
-  
-    const makeUppercase = (text) => {
-      log("Making uppercase");
-      timesRun.push(null);
-      return text.toUpperCase();
-    };
-  
-    return {
-      makeUppercase,
-      timesRun,
-    }
-  })();
-  
 
-  const counterCreator = () => {
-    let count = 0;
-    return () => {
-      console.log(count);
-      count++;
-    };
-  };
-  
-  const counter = counterCreator();
-  
+
+playGame()
